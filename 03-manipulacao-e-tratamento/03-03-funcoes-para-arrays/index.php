a66c2bb432dc511b87649a114cb28c1a8ce86fbf
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2020-11-11 11:00:23 -0300

    modulo 03 e aula 03

--- 03-manipulacao-e-tratamento/03-03-funcoes-para-arrays/index.php
+++ 03-manipulacao-e-tratamento/03-03-funcoes-para-arrays/index.php
@@ -7,20 +7,119 @@ fullStackPHPClassName("03.03 - Funções para arrays");
  */
 fullStackPHPClassSession("manipulação", __LINE__);
 
+$index = [
+    "AC\DC",
+    "Iron Maiden",
+    "Alter Bridge",
+];
+
+$assoc = [
+    "band_1" => "AC\DC",
+    "band_2" => "Iron Maiden",
+    "band_3" => "Alter Bridge",
+];
+
+array_unshift($index, "", "Pearl Jam");
+$assoc = ["band_4" => "Pearl Jam", "band_5" => ""] + $assoc;
+
+array_push($index, "");
+$assoc = $assoc + ["band_6" => ""];
+
+array_shift($index);
+array_shift($assoc);
+
+array_pop($index);
+array_pop($assoc);
+
+array_unshift($index, "");
+
+$index = array_filter($index);
+$assoc = array_filter($assoc);
+
+var_dump([
+    $index,
+    $assoc,
+]);
 
 /*
  * [ ordenação ] reverse | asort | ksort | sort
  */
 fullStackPHPClassSession("ordenação", __LINE__);
 
+$index = array_reverse($index);
+$assoc = array_reverse($assoc);
+
+asort($index);
+asort($assoc);
+
+ksort($index);
+ksort($assoc);
+
+krsort($index);
+krsort($assoc);
+
+sort($index);
+rsort($index);
+
+var_dump([
+    $index,
+    $assoc,
+]);
 
 /*
  * [ verificação ]  keys | values | in | explode
  */
 fullStackPHPClassSession("verificação", __LINE__);
 
+var_dump([
+    array_keys($assoc),
+    array_values($assoc),
+]);
+
+if (in_array("AC\DC", $assoc)) {
+    echo "<p>", "Cause I'm back", "</p>";
+}
+
+$arrToString = implode(", ", $assoc);
+echo "<p>Eu curto {$arrToString} e muitas outras bandas</p>";
+echo "<p>{$arrToString}</p>";
+
+$strToArray = explode(", ", $arrToString);
+var_dump($strToArray);
 
 /**
  * [ exemplo prático ] um template view | implode
  */
 fullStackPHPClassSession("exemplo prático", __LINE__);
+
+$profile = [
+    "name" => "Robson",
+    "company" => "UpInside",
+    "mail" => "cursos@upinside.com.br",
+];
+
+$template = <<<TPL
+    <article>
+        <h1>{{name}}</h1>
+        <p>
+            {{company}} <br>
+            <a href="mailto:{{mail}}" title="Enviar e-mail para {{name}}">Enviar E-mail</a>
+        </p>
+    </article>
+TPL;
+
+echo $template;
+
+echo str_replace(
+    array_keys($profile),
+    array_values($profile),
+    $template
+);
+
+$replaces = "{{" . implode("}}&{{", array_keys($profile)) . "}}";
+
+echo str_replace(
+    explode("&", $replaces),
+    array_values($profile),
+    $template
+);
