7a4ee2f681010404862a18761579238c7154ffb4
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2020-11-14 00:56:27 -0300

    add modulo04 aula 10

--- 04-php-orientado-a-objetos/04-10-fundamentos-de-abstracao/index.php
+++ 04-php-orientado-a-objetos/04-10-fundamentos-de-abstracao/index.php
@@ -1,4 +1,10 @@
 <?php
+
+use Source\App\User;
+use Source\Bank\Account;
+use Source\Bank\AccountCurrent;
+use Source\Bank\AccountSaving;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("04.10 - Fundamentos da abstração");
 
@@ -10,6 +16,17 @@ require __DIR__ . "/source/autoload.php";
  */
 fullStackPHPClassSession("superclass", __LINE__);
 
+$client = new User('Robson', 'Leite');
+/* $account = new Account(
+    '1600',
+    '22345',
+    $client,
+    1000
+); */
+var_dump(
+    // $account,
+    $client
+);
 
 /*
  * [ especialização ] É uma classe filha que implementa a superclasse e se especializa
@@ -17,9 +34,39 @@ fullStackPHPClassSession("superclass", __LINE__);
  */
 fullStackPHPClassSession("especialização.a", __LINE__);
 
+$saving = new AccountSaving(
+    '1600',
+    '22345',
+    $client,
+    1000
+);
+
+var_dump($saving);
+
+$saving->deposit(1000);
+$saving->withDraw(1500);
+$saving->withDraw(1006);
+
+$saving->extract();
 
 /*
  * [ especialização ] É uma classe filha que implementa a superclass e se especializa
  * com suas prórpias rotinas
  */
 fullStackPHPClassSession("especialização.b", __LINE__);
+
+$current = new AccountCurrent(
+    '1655',
+    '22346',
+    $client,
+    1000,
+    1000
+);
+
+var_dump($current);
+
+$current->deposit(1000);
+$current->withDraw(1500);
+$current->withDraw(1006);
+
+$current->extract();
