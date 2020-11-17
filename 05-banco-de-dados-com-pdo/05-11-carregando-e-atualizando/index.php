8872683e1c18d6aafb35c2e32dc4c17935b6778a
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2020-11-17 19:48:12 -0300

    add module 05-11

--- 05-banco-de-dados-com-pdo/05-11-carregando-e-atualizando/index.php
+++ 05-banco-de-dados-com-pdo/05-11-carregando-e-atualizando/index.php
@@ -1,4 +1,7 @@
 <?php
+
+use Source\Models\UserModel;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("05.11 - Carregando e atualizando");
 
@@ -9,4 +12,18 @@ require __DIR__ . "/../source/autoload.php";
  */
 fullStackPHPClassSession("save update", __LINE__);
 
+$model = new UserModel();
 
+$user = $model->load(2);
+$user->first_name = 'Kaue';
+$user->last_name = 'Oliveira';
+$user->email = 'kaue@upinside.com.br';
+
+if ($user != $model->load(2)) {
+    $user->save();
+    echo "<p class='trigger warning'>Atualizado!</p>";
+} else {
+    echo "<p class='trigger accept'>Nenhum dado foi alterado, usuário já atualizado</p>";
+}
+
+var_dump($user);
