9b1a95444d12f6bfde886803fcdde2c4c8ea1b15
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2020-11-17 11:57:51 -0300

    add module 05-04

--- 05-banco-de-dados-com-pdo/05-04-consultas-com-query-e-exec/index.php
+++ 05-banco-de-dados-com-pdo/05-04-consultas-com-query-e-exec/index.php
@@ -13,8 +13,30 @@ use Source\Database\Connect;
  * [ PDO exec ] http://php.net/manual/pt_BR/pdo.exec.php
  * [ PDO query ]http://php.net/manual/pt_BR/pdo.query.php
  */
+
 fullStackPHPClassSession("insert", __LINE__);
 
+$insert = "
+    INSERT INTO users (first_name, last_name, email, document)
+    VALUES ('Armando', 'Del Col', 'armando@upinside.com.br', '12365478978');
+";
+
+
+try {
+    // $exec = Connect::getInstance()->exec($insert);
+    // var_dump(Connect::getInstance()->lastInsertId());
+    $exec = null;
+
+    $query = Connect::getInstance()->query($insert);
+    var_dump(Connect::getInstance()->lastInsertId());
+
+    var_dump(
+        $exec,
+        $query
+    );
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
 
 /*
  * [ select ] Ler/Consultar dados.
@@ -22,6 +44,16 @@ fullStackPHPClassSession("insert", __LINE__);
  */
 fullStackPHPClassSession("select", __LINE__);
 
+try {
+    $query = Connect::getInstance()->query("SELECT * FROM users LIMIT 3");
+    var_dump([
+        $query,
+        $query->rowCount(),
+        $query->fetchAll()
+    ]);
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
 
 /*
  * [ update ] Atualizar dados.
@@ -29,9 +61,24 @@ fullStackPHPClassSession("select", __LINE__);
  */
 fullStackPHPClassSession("update", __LINE__);
 
+try {
+    $exec = Connect::getInstance()->exec("UPDATE users SET first_name = 'Kaue', last_name = 'Lourenço' WHERE id > '50'");
+
+    var_dump($exec);
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
 
 /*
  * [ delete ] Deletar dados.
  * https://mariadb.com/kb/en/library/delete/
  */
 fullStackPHPClassSession("delete", __LINE__);
+
+try {
+    $exec = Connect::getInstance()->exec("DELETE FROM users WHERE id > 50");
+
+    var_dump($exec);
+} catch (PDOException $exception) {
+    var_dump($exception);
+}
