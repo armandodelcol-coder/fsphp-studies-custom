6ecc2366628eda44218aec95d81419dee32f89b0
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2020-11-21 01:03:46 -0300

    add module 06 lecture 12

--- 06-seguranca-e-boas-praticas/source/Models/User.php
+++ 06-seguranca-e-boas-praticas/source/Models/User.php
@@ -100,6 +100,21 @@ class User extends Model
             return null;
         }
 
+        if (!is_email($this->email)) {
+            $this->message->warning('O e-mail informado não tem um formato válido');
+            return null;
+        }
+
+        if (!is_passwd($this->password)) {
+            $min = CONF_PASSWD_MIN_LEN;
+            $max = CONF_PASSWD_MAX_LEN;
+
+            $this->message->warning("A senha deve ter entre {$min} e {$max} caracteres");
+            return null;
+        } else {
+            $this->password = passwd($this->password);
+        }
+
         /** User Update */
         if (!empty($this->id)) {
             $userId = $this->id;
