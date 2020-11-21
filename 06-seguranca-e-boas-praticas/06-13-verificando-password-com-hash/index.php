35d388123f406b18e0d6136ef37dd970884d1b31
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2020-11-21 01:11:18 -0300

    add module 06 lecture 13

--- 06-seguranca-e-boas-praticas/06-13-verificando-password-com-hash/index.php
+++ 06-seguranca-e-boas-praticas/06-13-verificando-password-com-hash/index.php
@@ -1,4 +1,7 @@
 <?php
+
+use Source\Models\User;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("06.13 - Verificando password com hash");
 
@@ -8,3 +11,13 @@ require __DIR__ . "/../source/autoload.php";
  * [ hash ]
  */
 fullStackPHPClassSession("hash", __LINE__);
+
+$user = (new User())->findById(5);
+// $user->password = 12345678;
+$user->save();
+
+var_dump(
+    $user,
+    password_get_info('12345678'),
+    password_get_info(passwd('12345678'))
+);
